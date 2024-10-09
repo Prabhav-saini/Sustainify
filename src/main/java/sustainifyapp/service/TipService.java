86b3869be92e7ed9ee06a44a459023bc13209bbf
prabhavSaini <prabhavsaini02@gmail.com>
2024-10-09 23:24:36 +0530

    view tips feature

--- src/main/java/sustainifyapp/service/TipService.java
+++ src/main/java/sustainifyapp/service/TipService.java
@@ -1,7 +1,21 @@
 package sustainifyapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sustainifyapp.dao.TipDao;
+import sustainifyapp.model.Tip;
+
+import java.util.List;
 
 @Service
 public class TipService {
+
+    @Autowired
+    TipDao tipDao;
+
+    @Transactional
+    public List<Tip> viewAllTips() {
+        return tipDao.viewAllTips();
+    }
 }
